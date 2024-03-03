7766a606505b51b20bb55384660b07513b148679
evilevgeniys <903jenya@gmail.com>
2024-03-03 13:11:31 +0300

    finished filter and search functions

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,32 +1,7 @@
-import { Component } from 'react';
-
 import './employees-list-item.css'
 
-class EmployeesListItem extends Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            increase: false,
-            rise: false
-        }
-    }
-
-    onIncrease = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
-
-    onRise = () => {
-        this.setState(({rise}) => ({
-            rise: !rise
-        }))
-    }
-    
-    render() {
-        const {name, salary, onDelete} = this.props;
-        const {increase, rise} = this.state;
+const EmployeesListItem = (props) => { 
+        const {name, salary, onDelete, onToggleProp, increase, rise} = props;
 
         let classNames = "list-group-item d-flex justify-content-between";
         if(increase) {
@@ -39,12 +14,13 @@ class EmployeesListItem extends Component{
 
     return (
         <li className={classNames}>
-            <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+            <span className="list-group-item-label" onClick={onToggleProp} data-toggle='rise'>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + ' $'}/>
             <div className="d-flex justify-content-center align-items-center">
                 <button type="button" 
                     className="btn-cookie btn-sm"
-                    onClick={this.onIncrease}
+                    onClick={onToggleProp}
+                    data-toggle='increase'
                     >
                         <i className="fas fa-cookie"></i>
                 </button>
@@ -59,7 +35,7 @@ class EmployeesListItem extends Component{
             </div>
         </li>
     );
-    }
 }
+
 
 export default EmployeesListItem;
